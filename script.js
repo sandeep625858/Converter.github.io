4ee723b8439a4ef6c9cfb41ade763355ba7ef96b
sandeep625858 <108717062+sandeep625858@users.noreply.github.com>
2022-07-20 11:49:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function myFunction(){
     let gram = document.getElementById("grams").value;
     if(gram==""){
-        alert("You can't give blank")
+        alert("You need to insert a Number");
     }
     document.getElementById("KG").innerHTML = gram / 1000+"KG";
    
