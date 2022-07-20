88311372de37ede6f4f53a0c2dade1e19fe87c82
sandeep625858 <108717062+sandeep625858@users.noreply.github.com>
2022-07-20 12:12:18 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function myFunction(){
-    let gram = document.getElementById("grams").value;
-    if(gram==""){
-        alert("You need to insert a Number");
-    }
-    document.getElementById("KG").innerHTML = gram / 1000+"KG";
-   
-}
